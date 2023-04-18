072231ce88ae59f987c268d3adcb04607d089df9
jguneratne <59374962+jguneratne@users.noreply.github.com>
2023-04-18 07:42:37 -0400

    First attempt at making player marker show on square - returns undefined

--- script.js
+++ script.js
@@ -1,3 +1,36 @@
-const gameBoard = function () {
-  const gameMoves = [null, null, null, null, null, null, null, null, null];
+const gameBoard = (function () {
+  const gameMoves = ["", "", "", "", "", "", "", "", ""];
+
+  return { gameMoves };
+})();
+
+const player = function (marker) {
+  const makeMove = function () {
+    const squares = document.querySelectorAll("[data-index]");
+    squares.forEach((square) => {
+      square.addEventListener("pointerdown", function (e) {
+        const { target } = e;
+
+        if (!e.target.matches("[data-index]")) {
+          return;
+        } else if (e.target.matches(`[data-index="0"]`)) {
+          gameBoard.gameMoves.splice(0, 1, `${player.marker}`);
+          square.textContent = `${player.marker}`;
+          console.log(`${player.marker}`);
+        }
+      });
+    });
+  };
+  return { marker, makeMove };
 };
+
+const gamePlay = function () {
+  const player1 = player("X");
+  const player2 = player("O");
+
+  player1.makeMove();
+
+  return { player1 };
+};
+
+const playGame = gamePlay();
