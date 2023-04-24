b75fe17618903e6dbd09d62680909d628d48d07d
jguneratne <59374962+jguneratne@users.noreply.github.com>
2023-04-24 07:18:15 -0400

    Add checkWinner method and pdate placeMarker

--- script.js
+++ script.js
@@ -9,8 +9,6 @@ const gameBoard = (function () {
     gameMoves.push(square);
   });
 
-  //const gameMoves = Array.from(squares);
-
   return { squares, gameMoves };
 })();
 
@@ -23,12 +21,13 @@ function Player(name, marker) {
 function GamePlay() {
   const player1 = Player("Player1", "X");
   const player2 = Player("Player2", "O");
+  let winner = "";
 
-  console.log(player1.marker);
-  console.log(player2.marker);
+  //console.log(player1.marker);
+  //console.log(player2.marker);
 
   let activePlayer = player1;
-  // console.log(activePlayer.marker);
+  console.log(activePlayer);
 
   const switchPlayer = function () {
     if (activePlayer.marker === player1.marker) {
@@ -38,10 +37,23 @@ function GamePlay() {
     }
   };
 
+  const checkWinner = function () {
+    if (
+      gameBoard.gameMoves[(0, 1, 2)] === activePlayer.marker ||
+      gameBoard.gameMoves[(3, 4, 5)] === activePlayer.marker ||
+      gameBoard.gameMoves[(6, 7, 8)] === activePlayer.marker ||
+      gameBoard.gameMoves[(0, 4, 8)] === activePlayer.marker ||
+      gameBoard.gameMoves[(2, 4, 6)] === activePlayer.marker
+    ) {
+      winner = activePlayer;
+      console.log(`${activePlayer.name}` + " is the winner!");
+    }
+  };
+
   const placeMarker = function () {
     gameBoard.squares.forEach((square) => {
       square.addEventListener("pointerdown", function (e) {
-        if (e.target !== square || square.textContent !== "") {
+        if (e.target !== square || square.textContent !== "" || winner) {
           return;
         } else if (
           e.target === square &&
@@ -55,7 +67,10 @@ function GamePlay() {
             `${activePlayer.marker}`
           );
           console.log(gameBoard.gameMoves);
-          switchPlayer();
+          checkWinner();
+          if (!winner) {
+            switchPlayer();
+          }
         }
       });
     });
